d6f34ff5862e805001ae784b7d1558f89e0ff2b0
余佳妮 <yujiani@yujianideMacBook-Pro.local>
2020-08-15 17:31:11 -0500

    Add main.js file

---
+++ main.js
@@ -0,0 +1,6 @@
+function showAlert() {
+    var myElement = document.getElementById("intro");
+    var myText = myElement.textContent;
+    alert ("Hello world!"+myText);
+    myElement.style="color:green";
+  }
